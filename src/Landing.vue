72a378248cbc928f88a1ecf6fbb25801c381db45
baldychristophe <baldychristophe@gmail.com>
2020-06-16 22:05:11 +0200

    infinite scroll working finally

--- src/Landing.vue
+++ src/Landing.vue
@@ -1,13 +1,24 @@
 <template lang="pug">
 div
-  el-row.mb-4(v-for="draw in rawData" :key="draw")
-    el-card(shadow="hover")
-      .clearfix(slot="header") {{ draw.date }}
-      el-row(type="flex" justify="center")
-        div.mx-3(v-for="ball in draw.balls" :key="ball")
-          Ball(:num="ball")
-        div.mx-3(v-for="star in draw.stars" :key="star")
-          Star(:num="star")
+  .infinite-list-wrapper(style="overflow:auto")
+    ul(v-infinite-scroll="load")
+      li(v-for="draw in drawDisplayed" :key="draw.date")
+        el-card(shadow="hover")
+          .clearfix(slot="header") {{ draw.date }}
+          el-row(type="flex" justify="center")
+            div.mx-3(v-for="ball in draw.balls" :key="'ball' + draw.date + ball")
+              Ball(:num="ball")
+            div.mx-3(v-for="star in draw.stars" :key="'star' + draw.date + star")
+              Star(:num="star")
+
+  //- el-row.mb-4(v-for="draw in rawData" :key="draw")
+  //-   el-card(shadow="hover")
+  //-     .clearfix(slot="header") {{ draw.date }}
+  //-     el-row(type="flex" justify="center")
+  //-       div.mx-3(v-for="ball in draw.balls" :key="ball")
+  //-         Ball(:num="ball")
+  //-       div.mx-3(v-for="star in draw.stars" :key="star")
+  //-         Star(:num="star")
 
 </template>
 <script>
@@ -16,9 +27,20 @@ import Star from './components/Star'
 
 export default {
   name: 'landing',
+  data () {
+    return {
+      drawCount: 5,
+      draws: require('./data/test.json'),
+    }
+  },
   computed: {
-    rawData () {
-      return require('./data/test.json')
+    drawDisplayed () {
+      return this.draws.slice(0, this.drawCount)
+    },
+  },
+  methods: {
+    load () {
+      this.drawCount += 1
     },
   },
   components: {
@@ -36,5 +58,12 @@ export default {
 .card-body {
   font-size: x-small;
   padding: 10px;
+}
+.overflow-auto {
+  overflow: auto;
+  height: 500px;
+}
+.infinite-list-wrapper {
+  height: 500px;
 }
 </style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-el-menu(mode="horizontal" router="true" :default-active="activeIndex")
+el-menu(mode="horizontal" :router="true" :default-active="activeIndex")
   el-menu-item.menu-item(index="landing" :route="{ name: 'landing' }") Draws
   el-menu-item.menu-item(index="balls" :route="{ name: 'balls' }") Balls
   el-menu-item.menu-item(index="stars" :route="{ name: 'stars' }") Stars
