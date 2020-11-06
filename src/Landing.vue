4d2d00d64f2a3cef8ffee200a2579d2ac5ff56d2
baldychristophe <baldychristophe@gmail.com>
2020-11-06 20:42:59 +0100

    Landing: starting rework to split with draw component

--- src/Landing.vue
+++ src/Landing.vue
@@ -3,46 +3,12 @@
   div.row.mb-5.p-2.border.shadow-sm.pointer-hover(
     v-for="(draw, index) in drawDisplayed"
     :key="draw.date"
-    @click="rowClick(index)"
   )
-    .d-flex.align-items-center
-      div
-        table.table.text-small
-          thead
-            tr
-              th
-                div(v-if="index === 0") Last draw
-                div(v-else) {{ index + 1 }} draws ago
-                div {{ formattedDate(draw.date) }}
-              th(v-for="ball in draw.balls" :key="'ball' + draw.date + ball")
-                .d-flex.justify-content-center
-                  Ball(:num="ball")
-              th(v-for="star in draw.stars" :key="'star' + draw.date + star")
-                .d-flex.justify-content-center
-                  Star(:num="star")
-          tbody(class="collapse" :id="'drawStats' + index")
-            tr
-              th Occurrence
-              th.text-center(v-for="ball in draw.balls" :key="'ball' + draw.date + ball + 'ball_occurrence'") {{ draw.ball_occurrence[ball] }}
-              th.text-center(v-for="star in draw.stars" :key="'star' + draw.date + star + 'star_occurrence'") {{ draw.star_occurrence[star] }}
-
-            tr
-              th Last drawn
-              th.text-center(v-for="ball in draw.balls" :key="'ball' + draw.date + ball + 'ball_nb_draws_since_last_pick'") {{ draw.ball_nb_draws_since_last_pick[ball] }} draws ago
-              th.text-center(v-for="star in draw.stars" :key="'star' + draw.date + star + 'star_nb_draws_since_last_pick'") {{ draw.star_nb_draws_since_last_pick[star] }} draws ago
-      div.pr-2
-        <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-chevron-down" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
-        </svg>
+    Draw(:draw="draw" :index="index")
 
 </template>
 <script>
-import Ball from './components/Ball'
-import Star from './components/Star'
-
-import { formattedDate } from './utils'
-
-import $ from 'jquery'
+import Draw from './components/Draw'
 
 export default {
   name: 'landing',
@@ -58,12 +24,8 @@ export default {
     },
   },
   methods: {
-    formattedDate,
     load () {
       this.drawCount += 1
-    },
-    rowClick (rowIndex) {
-      $('#drawStats' + rowIndex).collapse('toggle')
     },
   },
   mounted () {
@@ -74,36 +36,11 @@ export default {
     }
   },
   components: {
-    Ball,
-    Star,
+    Draw,
   },
 }
 </script>
 <style lang="scss" scoped>
-table {
-  margin-bottom: 0;
-}
-thead th {
-  border-top: 0;
-}
-.table thead th {
-  border-bottom: 0;
-}
-thead, tbody tr {
-  display:table;
-  width:100%;
-  table-layout:fixed;
-}
-.collapsing {
-  display: block;
-  height: 0;
-  overflow: hidden;
-
-  -webkit-transition: height .35s ease;
-  -moz-transition: height .35s ease;
-  -o-transition: height .35s ease;
-  transition: height .35s ease;
-}
 .draw-shadow {
   box-shadow: 0 1px 5px 2px rgba(0, 0, 0, 0.05);
 }
---
+++ src/components/Draw.vue
@@ -0,0 +1,84 @@
+<template lang="pug">
+.d-flex.align-items-center(@click="rowClick(index)")
+  div
+    table.table.text-small
+      thead
+        tr
+          th
+            div(v-if="index === 0") Last draw
+            div(v-else) {{ index + 1 }} draws ago
+            div {{ formattedDate(draw.date) }}
+          th(v-for="ball in draw.balls" :key="'ball' + draw.date + ball")
+            .d-flex.justify-content-center
+              Ball(:num="ball")
+          th(v-for="star in draw.stars" :key="'star' + draw.date + star")
+            .d-flex.justify-content-center
+              Star(:num="star")
+      tbody(class="collapse" :id="'drawStats' + index")
+        tr
+          th Occurrence
+          th.text-center(v-for="ball in draw.balls" :key="'ball' + draw.date + ball + 'ball_occurrence'") {{ draw.ball_occurrence[ball] }}
+          th.text-center(v-for="star in draw.stars" :key="'star' + draw.date + star + 'star_occurrence'") {{ draw.star_occurrence[star] }}
+
+        tr
+          th Last drawn
+          th.text-center(v-for="ball in draw.balls" :key="'ball' + draw.date + ball + 'ball_nb_draws_since_last_pick'") {{ draw.ball_nb_draws_since_last_pick[ball] }} draws ago
+          th.text-center(v-for="star in draw.stars" :key="'star' + draw.date + star + 'star_nb_draws_since_last_pick'") {{ draw.star_nb_draws_since_last_pick[star] }} draws ago
+  div.pr-2
+    <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-chevron-down" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+      <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+    </svg>
+</template>
+
+<script>
+import Ball from './Ball'
+import Star from './Star'
+
+import { formattedDate } from '../utils'
+
+import $ from 'jquery'
+
+export default {
+  name: 'draw',
+  props: {
+    draw: Object,
+    index: Number,
+  },
+  methods: {
+    formattedDate,
+    rowClick (rowIndex) {
+      $('#drawStats' + rowIndex).collapse('toggle')
+    },
+  },
+  components: {
+    Ball,
+    Star,
+  },
+}
+</script>
+<style lang="scss" scoped>
+table {
+  margin-bottom: 0;
+}
+thead th {
+  border-top: 0;
+}
+.table thead th {
+  border-bottom: 0;
+}
+thead, tbody tr {
+  display:table;
+  width:100%;
+  table-layout:fixed;
+}
+.collapsing {
+  display: block;
+  height: 0;
+  overflow: hidden;
+
+  -webkit-transition: height .35s ease;
+  -moz-transition: height .35s ease;
+  -o-transition: height .35s ease;
+  transition: height .35s ease;
+}
+</style>
