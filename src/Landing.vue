3961d9dc841729657e389247b88d64d78d7d7c2a
baldychristophe <baldychristophe@gmail.com>
2020-11-06 19:48:45 +0100

    update landing page

--- src/Landing.vue
+++ src/Landing.vue
@@ -1,30 +1,39 @@
 <template lang="pug">
 .container
-  div(v-for="(draw, index) in drawDisplayed" :key="draw.date")
-    div.mt-5.border.rounded.draw-shadow(@click="rowClick(index)")
-      table.table.text-small
-        thead
-          tr
-            th
-              div(v-if="index === 0") Last draw
-              div(v-else) {{ index + 1 }} draws ago
-              div {{ formattedDate(draw.date) }}
-            th(v-for="ball in draw.balls" :key="'ball' + draw.date + ball")
-              .d-flex.justify-content-center
-                Ball(:num="ball")
-            th(v-for="star in draw.stars" :key="'star' + draw.date + star")
-              .d-flex.justify-content-center
-                Star(:num="star")
-        tbody(class="collapse" :id="'drawStats' + index")
-          tr
-            th Occurrence
-            th.text-center(v-for="ball in draw.balls" :key="'ball' + draw.date + ball + 'ball_occurrence'") {{ draw.ball_occurrence[ball] }}
-            th.text-center(v-for="star in draw.stars" :key="'star' + draw.date + star + 'star_occurrence'") {{ draw.star_occurrence[star] }}
+  div.row.mb-5.p-2.border.shadow-sm.pointer-hover(
+    v-for="(draw, index) in drawDisplayed"
+    :key="draw.date"
+    @click="rowClick(index)"
+  )
+    .d-flex.align-items-center
+      div
+        table.table.text-small
+          thead
+            tr
+              th
+                div(v-if="index === 0") Last draw
+                div(v-else) {{ index + 1 }} draws ago
+                div {{ formattedDate(draw.date) }}
+              th(v-for="ball in draw.balls" :key="'ball' + draw.date + ball")
+                .d-flex.justify-content-center
+                  Ball(:num="ball")
+              th(v-for="star in draw.stars" :key="'star' + draw.date + star")
+                .d-flex.justify-content-center
+                  Star(:num="star")
+          tbody(class="collapse" :id="'drawStats' + index")
+            tr
+              th Occurrence
+              th.text-center(v-for="ball in draw.balls" :key="'ball' + draw.date + ball + 'ball_occurrence'") {{ draw.ball_occurrence[ball] }}
+              th.text-center(v-for="star in draw.stars" :key="'star' + draw.date + star + 'star_occurrence'") {{ draw.star_occurrence[star] }}
 
-          tr
-            th Draws since last
-            th.text-center(v-for="ball in draw.balls" :key="'ball' + draw.date + ball + 'ball_nb_draws_since_last_pick'") {{ draw.ball_nb_draws_since_last_pick[ball] }}
-            th.text-center(v-for="star in draw.stars" :key="'star' + draw.date + star + 'star_nb_draws_since_last_pick'") {{ draw.star_nb_draws_since_last_pick[star] }}
+            tr
+              th Last drawn
+              th.text-center(v-for="ball in draw.balls" :key="'ball' + draw.date + ball + 'ball_nb_draws_since_last_pick'") {{ draw.ball_nb_draws_since_last_pick[ball] }} draws ago
+              th.text-center(v-for="star in draw.stars" :key="'star' + draw.date + star + 'star_nb_draws_since_last_pick'") {{ draw.star_nb_draws_since_last_pick[star] }} draws ago
+      div.pr-2
+        <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-chevron-down" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+        </svg>
 
 </template>
 <script>
@@ -71,6 +80,9 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+table {
+  margin-bottom: 0;
+}
 thead th {
   border-top: 0;
 }
