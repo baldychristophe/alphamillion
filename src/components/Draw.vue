06b3dd345278fdc3388f557516380d6a7c46db22
baldychristophe <baldychristophe@gmail.com>
2020-11-07 20:57:15 +0100

    Revert "rework with bootstrap grid"
    This reverts commit c45a1863a0c3ed1e401446827b2ac3e31a5c41a7.

--- src/components/Draw.vue
+++ src/components/Draw.vue
@@ -1,17 +1,17 @@
 <template lang="pug">
 .p-4.border.shadow-sm.pointer-hover.text-small.w-100(@click="rowClick(index)")
-  .row
-    div.col(v-if="index === 0") Last draw
-    div.co(v-else) {{ index + 1 }} draws ago
-    div.col {{ formattedDate(draw.date) }}
-    div.col(v-for="ball in draw.balls" :key="'ball' + draw.date + ball")
+  .d-flex.align-items-center
+    div.flex-fill(v-if="index === 0") Last draw
+    div.flex-fill(v-else) {{ index + 1 }} draws ago
+    div.flex-fill {{ formattedDate(draw.date) }}
+    div.flex-fill(v-for="ball in draw.balls" :key="'ball' + draw.date + ball")
       .d-flex.justify-content-center
         Ball(:num="ball")
-    div.col(v-for="star in draw.stars" :key="'star' + draw.date + star")
+    div.flex-fill(v-for="star in draw.stars" :key="'star' + draw.date + star")
       .d-flex.justify-content-center
         Star(:num="star")
 
-    div.col
+    div.flex-fill
       div.float-right(v-if="!isOpen")
         <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-chevron-up" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
           <path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"/>
@@ -22,15 +22,15 @@
         </svg>
 
   div(class="collapse" :id="'drawStats' + index")
-    div.row.mt-4.border-top.py-3
-      div.col-2 Occurrence
-      div.col.text-center(v-for="ball in draw.balls" :key="'ball' + draw.date + ball + 'ball_occurrence'") {{ draw.ball_occurrence[ball] }}
-      div.col.text-center(v-for="star in draw.stars" :key="'star' + draw.date + star + 'star_occurrence'") {{ draw.star_occurrence[star] }}
+    div.d-flex.align-items-center.mt-4.border-top.py-3
+      div.flex-fill Occurrence
+      div.flex-fill.text-center(v-for="ball in draw.balls" :key="'ball' + draw.date + ball + 'ball_occurrence'") {{ draw.ball_occurrence[ball] }}
+      div.flex-fill.text-center(v-for="star in draw.stars" :key="'star' + draw.date + star + 'star_occurrence'") {{ draw.star_occurrence[star] }}
 
-    div.row.border-top.py-3
-      div.col-2 Last drawn
-      div.col.text-center(v-for="ball in draw.balls" :key="'ball' + draw.date + ball + 'ball_nb_draws_since_last_pick'") {{ draw.ball_nb_draws_since_last_pick[ball] }} draws ago
-      div.col.text-center(v-for="star in draw.stars" :key="'star' + draw.date + star + 'star_nb_draws_since_last_pick'") {{ draw.star_nb_draws_since_last_pick[star] }} draws ago
+    div.d-flex.align-items-center.border-top.py-3
+      div.flex-fill Last drawn
+      div.flex-fill.text-center(v-for="ball in draw.balls" :key="'ball' + draw.date + ball + 'ball_nb_draws_since_last_pick'") {{ draw.ball_nb_draws_since_last_pick[ball] }} draws ago
+      div.flex-fill.text-center(v-for="star in draw.stars" :key="'star' + draw.date + star + 'star_nb_draws_since_last_pick'") {{ draw.star_nb_draws_since_last_pick[star] }} draws ago
 
 </template>
 
