e12dff3b155f18aa3d892972231ed2e0b3bce12e
baldychristophe <baldychristophe@gmail.com>
2020-11-08 17:05:52 +0100

    Add draws metadata

--- src/Landing.vue
+++ src/Landing.vue
@@ -1,11 +1,25 @@
 <template lang="pug">
 .container
+  .row.mb-5
+    .col.d-flex.flex-column.text-center
+      div.text-small First draw*
+      div {{ formattedDate(drawsMetadata.first_draw) }}
+    .col.d-flex.flex-column.text-center
+      div.text-small Last draw
+      div {{ formattedDate(drawsMetadata.last_draw) }}
+    .col.d-flex.flex-column.text-center
+      div.text-small Draws included
+      div {{ drawsMetadata.number_of_draws }}
+
+  .row.mb-3
+    .col.text-xsmall *In September 2016 the Lucky Star ball pool was increased from 11 to 12
   .row.mb-5(v-for="(draw, index) in drawDisplayed" :key="draw.date")
     Draw(:draw="draw" :index="index")
 
 </template>
 <script>
 import Draw from './components/Draw'
+import { formattedDate } from './utils'
 
 export default {
   name: 'landing',
@@ -13,6 +27,7 @@ export default {
     return {
       drawCount: 10,
       draws: require('./data/draws_with_stats.json'),
+      drawsMetadata: require('./data/draws_metadata.json'),
     }
   },
   computed: {
@@ -21,6 +36,7 @@ export default {
     },
   },
   methods: {
+    formattedDate,
     load () {
       this.drawCount += 1
     },
