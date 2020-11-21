1c61fb158f76426fada32b55b6f8eb539bd3f317
baldychristophe <baldychristophe@gmail.com>
2020-11-21 20:35:32 +0100

    Extract background color gradient functions from Draw into utils

--- src/components/Draw.vue
+++ src/components/Draw.vue
@@ -103,7 +103,12 @@ import Ball from './Ball'
 import Star from './Star'
 import DrawHeatmap from './DrawHeatmap'
 
-import { formattedDate, drawAgoText } from '../utils'
+import {
+  formattedDate,
+  drawAgoText,
+  backgroundColorGradientOccurrence,
+  backgroundColorGradientDrawGap,
+} from '../utils'
 
 import $ from 'jquery'
 
@@ -127,55 +132,11 @@ export default {
   methods: {
     formattedDate,
     drawAgoText,
+    backgroundColorGradientOccurrence,
+    backgroundColorGradientDrawGap,
     rowClick (rowIndex) {
       $('#drawStats' + rowIndex).collapse('toggle')
       this.isCollapseOpen = !this.isCollapseOpen
-    },
-    backgroundColorGradientOccurrence (num, expected) {
-      const diff = num / expected
-      if (diff === 1) {
-        return ''
-      } else if (diff > 1 && diff <= 1.05) {
-        return 'bg-green-3'
-      } else if (diff > 1.05 && diff <= 1.10) {
-        return 'bg-green-2'
-      } else if (diff > 1.10 && diff <= 1.20) {
-        return 'bg-green-1'
-      } else if (diff > 1.20) {
-        return 'bg-green-0'
-      } else if (diff < 1 && diff >= 0.95) {
-        return 'bg-red-3'
-      } else if (diff < 0.95 && diff >= 0.90) {
-        return 'bg-red-0'
-      } else if (diff < 0.90 && diff >= 0.80) {
-        return 'bg-red-0'
-      } else if (diff < 0.80) {
-        return 'bg-red-0'
-      }
-      return ''
-    },
-    backgroundColorGradientDrawGap (num, expected) {
-      const diff = num - expected
-      if (diff === 0) {
-        return ''
-      } else if (diff > 0 && diff <= 5) {
-        return 'bg-green-3'
-      } else if (diff > 5 && diff <= 10) {
-        return 'bg-green-2'
-      } else if (diff > 10 && diff <= 20) {
-        return 'bg-green-1'
-      } else if (diff > 20) {
-        return 'bg-green-0'
-      } else if (diff < 0 && diff >= -2) {
-        return 'bg-red-3'
-      } else if (diff < -2 && diff >= -5) {
-        return 'bg-red-2'
-      } else if (diff < -5 && diff >= -8) {
-        return 'bg-red-1'
-      } else if (diff < -8) {
-        return 'bg-red-0'
-      }
-      return ''
     },
   },
   mounted () {
--- src/utils.js
+++ src/utils.js
@@ -7,3 +7,51 @@ export function formattedDate (date) {
 export function drawAgoText (num) {
   return `${num} ${num === 1 ? 'draw' : 'draws'} ago`
 }
+
+export function backgroundColorGradientOccurrence (ballOccurrence, expectedOccurrence) {
+  const diff = ballOccurrence / expectedOccurrence
+  if (diff === 1) {
+    return ''
+  } else if (diff > 1 && diff <= 1.05) {
+    return 'bg-green-3'
+  } else if (diff > 1.05 && diff <= 1.10) {
+    return 'bg-green-2'
+  } else if (diff > 1.10 && diff <= 1.20) {
+    return 'bg-green-1'
+  } else if (diff > 1.20) {
+    return 'bg-green-0'
+  } else if (diff < 1 && diff >= 0.95) {
+    return 'bg-red-3'
+  } else if (diff < 0.95 && diff >= 0.90) {
+    return 'bg-red-0'
+  } else if (diff < 0.90 && diff >= 0.80) {
+    return 'bg-red-0'
+  } else if (diff < 0.80) {
+    return 'bg-red-0'
+  }
+  return ''
+}
+
+export function backgroundColorGradientDrawGap (ballLastDraw, expectedDrawGap) {
+  const diff = ballLastDraw - expectedDrawGap
+  if (diff === 0) {
+    return ''
+  } else if (diff > 0 && diff <= 5) {
+    return 'bg-green-3'
+  } else if (diff > 5 && diff <= 10) {
+    return 'bg-green-2'
+  } else if (diff > 10 && diff <= 20) {
+    return 'bg-green-1'
+  } else if (diff > 20) {
+    return 'bg-green-0'
+  } else if (diff < 0 && diff >= -2) {
+    return 'bg-red-3'
+  } else if (diff < -2 && diff >= -5) {
+    return 'bg-red-2'
+  } else if (diff < -5 && diff >= -8) {
+    return 'bg-red-1'
+  } else if (diff < -8) {
+    return 'bg-red-0'
+  }
+  return ''
+}
